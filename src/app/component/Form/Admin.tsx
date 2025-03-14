37d560cbb38eb2af10cc250401e8313809357e3e
Karthika <karthika3-k>
2025-03-14 14:26:42 +0530

    Add Admin page

--- src/app/component/Form/Admin.tsx
+++ src/app/component/Form/Admin.tsx
@@ -1,8 +1,18 @@
+import UserCard from "../UserCard/UserCard";
+
+
 const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-row">
             {/* Left */}
             <div className="w-full lg:w-2/3">L</div>
+            {/* UserCard */}
+            <div className="flex gap-4 justify-between">
+                <UserCard type="Inward" />
+                <UserCard type="Outward" />
+                <UserCard type="Loading" />
+                <UserCard type="UnLoading" />
+            </div>
             {/* right */}
             <div className="w-full lg:w-2/3">R</div>
         </div>
---
+++ src/app/component/UserCard/UserCard.tsx
@@ -0,0 +1,12 @@
+    
+const UserCard = ({ type }: { type: string }) => {
+    return (
+        <div className='rounded-2xl odd:bg-purple-200 even:bg-yellow-200 p-4 flex-1'>
+            <div className="">
+                <span>2024/25</span>
+                
+            </div>
+        </div>
+    );
+};
+export default UserCard;
