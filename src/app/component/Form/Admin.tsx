b2bf9f69180a2f44d5b19a37c629009ebaee12c2
Karthika <karthika3-k>
2025-03-14 15:15:18 +0530

    count chart code changes

--- src/app/component/Form/Admin.tsx
+++ src/app/component/Form/Admin.tsx
@@ -5,16 +5,17 @@ const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-row">
             {/* Left */}
-            <div className="w-full lg:w-2/3">L</div>
+            <div className="w-full lg:w-2/3">
             {/* UserCard */}
-            <div className="flex gap-4 justify-between">
+            <div className="flex gap-4 justify-between flex-wrap">
                 <UserCard type="Inward" />
                 <UserCard type="Outward" />
                 <UserCard type="Loading" />
                 <UserCard type="UnLoading" />
             </div>
+            </div>
             {/* right */}
-            <div className="w-full lg:w-2/3">R</div>
+            <div className="w-full lg:w-1/3">R</div>
         </div>
     );
 };
